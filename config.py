33390f53991759754a8eca4f9835d9a577553191
Денис Сушков <ga.akam@yandex.ru>
2023-08-09 18:15:29 +0300

    test bd and secret_key

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', default='123456789')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='my_secret_key')
     PEOPLE_FOLDER = os.path.join('app/static', 'people_photo')
     FLASK_ADMIN_SWATCH = 'superhero'
     ADMIN_NAME = os.getenv('ADMIN_NAME', default='superhero')
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,19 +1,22 @@
 import os
 
+from dotenv import load_dotenv
 
-def test_env_vars():
-    assert 'sqlite:///db.sqlite3' in list(os.environ.values()), (
-        'Проверьте наличие переменной окружения с настройками для подключения'
-        ' базы данных со значением sqlite:///db.sqlite3'
+from config import Config
+
+load_dotenv()
+
+
+def test_database():
+    assert Config.SQLALCHEMY_DATABASE_URI == os.getenv('SQLALCHEMY_DATABASE_URI'), (
+        'Проверьте переменную окружения '
+        'SQLALCHEMY_DATABASE_URI в файле .env '
+        'она должна быть равна: sqlite:///db.sqlite3'
     )
 
 
-def test_config(default_app):
-    assert default_app.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///db.sqlite3', (
-        'Проверьте, что конфигурационному ключу SQLALCHEMY_DATABASE_URI '
-        'присвоено значение с настройками для подключения базы данных'
-    )
-    assert default_app.config['SECRET_KEY'] == os.getenv('SECRET_KEY'), (
-        'Проверьте, что конфигурационному ключу SECRET_KEY '
-        'присвоено значение'
+def test_secret_key():
+    assert Config.SECRET_KEY == os.getenv('SECRET_KEY'), (
+        'Проверьте переменную окружения '
+        'SECRET_KEY в файле .env '
     )
